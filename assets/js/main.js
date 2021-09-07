b4361bbbe8f588705ca8d7c5e04b501441b7ff67
zemanjirka <zemanjirka@gmail.com>
2021-09-07 09:41:05 +0000

    add name in js. add year in js.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 let date = document.getElementById('date');
 
 
-let options = {day:'numeric', weekday:'long', month:'long',};
+let options = {day:'numeric', weekday:'long', month:'long', year: 'numeric', };
 let currentDate = new Date();
 
 date.innerHTML = currentDate.toLocaleDateString('en-US', options);
+
+function playerName() {
+    var name = document.getElementById('name-input').value;
+
+    if (name != null) {
+        document.getElementById('player').innerHTML = `Welcome ${name}, let's do some KREPSKO stuff!`;
+    }
+}
